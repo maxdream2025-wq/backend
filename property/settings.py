94f5845a3229f5b9a50a3794152cf3f4e3014273
Basat Ali <cool.saa733@gmail.comm>
2025-10-10 18:31:07 +0400

    added

--- property/settings.py
+++ property/settings.py
@@ -144,11 +144,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "https://remax-backend.onrender.com"
-]
+CORS_ALLOWED_ORIGINS = ['*']
 CORS_ALLOW_ALL_ORIGINS = True
 
 
